43ff6f17a54c751baf601adb1e37fe0578e40813
郭斐靓 <2398578534@qq.com>
2022-03-08 08:30:59 +0800

    todo vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="todoapp">
+    <header class="header">
+      <h1>todos</h1>
+      <input type="text" class="new-todo" v-model="newTodo"
+      @keyup.enter="addTodo"
+      />
+    </header>
+    <section class="main">
+      <input type="checkbox" id="toggle-all" class="toggle-all" v-model="isAll"/>
+      <label for="toggle-all"></label>
+      <ul class="todo-list">
+        <HelloWorld v-for="value in todoDatas" :key="value.id" :todo="value"/>
+       
+      </ul>
+    </section>
+    <HelloFooter />
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import HelloFooter from "./components/HelloFooter.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    HelloFooter
+  },
+  data() {
+    return {
+      todoDatas:[],
+      newTodo:""
+    }
+  },
+  methods:{
+      addTodo(){
+      if(this.newTodo.trim=="") return ;
+      let id=new Date().getTime();
+      let title=this.newTodo;
+      this.todoDatas.push({id:id,title:title,hasCompleted:false}),
+      this.newTodo="";
+
+    }
+  },
+   computed:{
+      isAll:{
+        get(){
+          return false;
+        },
+        set(value){
+          this.todoDatas.map((todo)=>{
+                todo.hasCompleted=value;
+                return todo;
+            });
+        }
+      }
+    }
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/HelloFooter.vue
@@ -0,0 +1,34 @@
+<template>
+    <footer class="footer">
+        <span class="todo-count">
+        <span>{{ todoNum}}</span>
+        <span v-if="todoNum<=1">  items left</span>
+        <span v-else>item left</span>
+        </span>
+        <ul class="filters">
+            <li><a href="#/all" class="selected">All</a></li>
+             <li><a href="#/active"></a>Active</li>
+              <li><a href="#/completed"></a>Comppleted</li>
+        </ul>
+        <button class="clear-completed">Clear Completed</button>
+    </footer>
+</template>
+
+<script>
+
+    export default {
+    
+        name:"HelloFooter",
+          computed:{
+            todoNum(){
+                return this.$parent.todoDatas.filter((value)=>{
+                    return !value.hasCompleted;
+                }).length;
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
